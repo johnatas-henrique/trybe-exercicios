f0a1b65a10248783cfbf09f66cfa5c2b21b38ad4
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-10 20:06:07 -0300

    Exercicios 4 e 5

---
+++ 8_4/exercise5.js
@@ -0,0 +1,25 @@
+const assert = require('assert')
+
+// escreva aqui o seu código
+class Dog {
+    constructor(name) {
+        this.name = name;
+        this.bark = function () {
+            return `${this.name} is barking`;
+        };
+    }
+}
+
+let dog1 = new Dog("Apolo")
+
+assert.equal(typeof Dog.prototype, "object")
+assert.ok(dog1 instanceof Dog)
+assert.equal(dog1.name, "Apolo")
+assert.equal(dog1.bark(), "Apolo is barking")
+
+let dog2 = new Dog("Arcos")
+
+assert.equal(typeof Dog.prototype, "object")
+assert.ok(dog2 instanceof Dog)
+assert.equal(dog2.name, "Arcos")
+assert.equal(dog2.bark(), "Arcos is barking")
