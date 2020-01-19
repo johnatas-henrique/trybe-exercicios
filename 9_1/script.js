d77d78d3067e0e92b0f9af1495fdc9cc7652d7b9
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-18 23:20:15 -0300

    Exercício finalizado, partindo para o bônus

--- 9_1/script.js
+++ 9_1/script.js
@@ -12,7 +12,11 @@ const trocaFonteTamanho = () => divAlterar.style.fontSize = `${fonteTamanho.valu
 const trocaLinhaAltura = () => divAlterar.style.lineHeight = `${linhaAltura.value}px`;
 const trocaFonteFamilia = () => divAlterar.style.fontFamily = fonteFamilia.value;
 const salvarLocalStorage = () => {
-    console.log('botão para salvar');
+    localStorage.setItem('bgCor', bgCor.value);
+    localStorage.setItem('textoCor', textoCor.value);
+    localStorage.setItem('fonteTamanho', fonteTamanho.value);
+    localStorage.setItem('linhaAltura', linhaAltura.value);
+    localStorage.setItem('fonteFamilia', fonteFamilia.value);
 }
 
 
@@ -22,3 +26,16 @@ fonteTamanho.addEventListener('change', trocaFonteTamanho);
 linhaAltura.addEventListener('change', trocaLinhaAltura);
 fonteFamilia.addEventListener('change', trocaFonteFamilia);
 btnSalvar.addEventListener('click', salvarLocalStorage);
+
+
+const locBgCor = localStorage.getItem('bgCor');
+const locTextoCor = localStorage.getItem('textoCor');
+const locFonteTamanho = localStorage.getItem('fonteTamanho');
+const locLinhaAltura = localStorage.getItem('linhaAltura');
+const locFonteFamilia = localStorage.getItem('fonteFamilia');
+
+if (locBgCor) divAlterar.style.backgroundColor = locBgCor;
+if (locTextoCor) divAlterar.style.color = locTextoCor;
+if (locFonteTamanho) divAlterar.style.fontSize = `${locFonteTamanho}px`;
+if (locLinhaAltura) divAlterar.style.lineHeight = `${locLinhaAltura}px`;
+if (locFonteFamilia) divAlterar.style.fontFamily = locFonteFamilia;
