f4c9ad3d6326683d1382291467983dca8827b698
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-30 19:37:13 -0300

    Pokedex

---
+++ 11_2/my-pokedex/src/Pokemon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+class Pokemon extends React.Component {
+    render () {
+        const {id, name, type, weight, unit, image, info} = this.props;
+        return (
+        <div className = 'poke-square'>
+            <div className='w-60'>
+                <h2>#{id} - {name}</h2>
+                <p>{type}</p>
+                <p>Peso médio: {weight}{unit}</p>
+                <p><a href={info} target='_blank'>Mais info</a></p>
+            </div>
+            <div className='w-40'>
+                <img src={image} alt={name}></img>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default Pokemon;
