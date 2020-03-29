5ec41004417871315a3927fb4b81044b54f42e2f
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-03-29 13:49:54 -0300

    Exercício 2 concluído

--- 9_3/apiScript.js
+++ 9_3/apiScript.js
@@ -1,18 +1,16 @@
 const API_URL = 'https://icanhazdadjoke.com/search';
 
 const header = {
-    headers: { Accept: 'application/json' }
+  headers: { Accept: 'application/json' };
 };
 
 const fetchJoke = () => {
-    fetch(API_URL, header)
-    .then(response => {
-        response.json()
-        .then(response => {
-            const random = Math.floor(Math.random()*20)
-            document.getElementById('jokeContainer').innerHTML 
-            = `${random} - ${response.results[random].joke}`;
-        })
+  fetch(API_URL, header).then(response => {
+    response.json().then(response => {
+      const random = Math.floor(Math.random() * 20);
+      const jokeDiv = document.getElementById('jokeContainer');
+      jokeDiv.innerHTML = `${random} - ${response.results[random].joke}`;
     })
-} 
+  })
+}
 fetchJoke()
