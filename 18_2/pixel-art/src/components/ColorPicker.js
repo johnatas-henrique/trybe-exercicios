faae8128f6de2918ea0abb6e537841cb3b236d29
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-04-04 16:54:25 -0300

    Apps in Context mode

---
+++ 18_2/pixel-art/src/components/ColorPicker.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { PainterContext } from "../contexts/PainterContext";
+
+function ColorPicker(_) {
+  const { colors, setColor } = React.useContext(PainterContext);
+
+  const colorOption = color => {
+    return (
+      <div
+        key={color.name}
+        className="color"
+        style={{ backgroundColor: color.hex }}
+        onClick={() => setColor(color)}
+      />
+    );
+  };
+
+  const colorOptions = () => {
+    return colors.map(color => {
+      return colorOption(color);
+    });
+  };
+
+  return <div className="colorpicker">{colorOptions()}</div>;
+}
+
+export default ColorPicker;
