426856219166f5788527347c77ed4fd3572ceb06
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-09-03 18:41:31 -0300

    Exercício 1

---
+++ 36_1/area.py
@@ -0,0 +1,18 @@
+PI = 3.14
+
+
+def square(lado):
+    return lado ** 2
+
+
+def rectangle(base, altura):
+    return base * altura
+
+
+def circle(raio):
+    return PI * raio * raio
+
+if __name__ == "__main__":
+    print("Área do quadrado:", square(8))
+    print("Área do retângulo:", rectangle(2, 2))
+    print("Área do círculo:", circle(4))
---
+++ 36_1/ex1.py
@@ -0,0 +1,9 @@
+def maior(num1, num2):
+    if (num1 > num2):
+        return num1
+
+    return num2
+
+
+print(maior(5, 4))
+print(maior(50, 400))
