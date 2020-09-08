66e873ca828e3b72669fb44a14e91795135214ad
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-09-08 18:16:06 -0300

    Exercício 2

---
+++ 36_3/ex2.py
@@ -0,0 +1,20 @@
+import random
+
+LISTA = ['JOHNATAS', 'THAWANE', 'AYRTON', 'FELIPE']
+
+escolhida = random.choice(LISTA)
+
+escolhida_embaralhada = "".join(random.sample(escolhida, len(escolhida)))
+
+count = 0
+
+while count < 3:
+    print("A palavra embaralhada é: ", escolhida_embaralhada)
+    tentativa = input("Digite a palavra corretamente: ")
+    if (tentativa == escolhida):
+        print(f"Você acertou na {count} tentativa")
+    else:
+        print("Você errou, tente novamentes")
+    count += 1
+    if (count == 3):
+        print("Acabaram as tentativas, valeu!")
