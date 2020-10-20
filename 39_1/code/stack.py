3755e1b9667666fb06644f85bd8ebe00a6b0b331
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-10-20 18:26:54 -0300

    Exercício 1

---
+++ 39_1/code/stack.py
@@ -0,0 +1,38 @@
+class Stack():
+    def __init__(self):
+        self._data = list()
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
+
+    def push(self, value):
+        self._data.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        value = self._data[-1]
+        del self._data[-1]
+        return value
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        value = self._data[-1]
+        return value
+
+    def clear(self):
+        self._data.clear()
+
+    def __str__(self):
+        str_items = ""
+        for i in range(self.size()):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < self.size():
+                str_items += ", "
+
+        return "Stack(" + str_items + ")"
